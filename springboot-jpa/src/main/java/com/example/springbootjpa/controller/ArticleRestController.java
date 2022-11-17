113dc6b6f9d39c30e653c05ab722aeb38d53e33f
Kimkiheon <cbj2741@gmail.com>
2022-11-17 13:16:22 +0900

    ADD: ArticleRestController

---
+++ springboot-jpa/src/main/java/com/example/springbootjpa/controller/ArticleRestController.java
@@ -0,0 +1,21 @@
+package com.example.springbootjpa.controller;
+
+import com.example.springbootjpa.domain.dto.ArticleResponse;
+import com.example.springbootjpa.domain.repository.ArticleRepository;
+import com.example.springbootjpa.service.ArticleService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+public class ArticleRestController {
+    private final ArticleService articleService;
+
+    public ArticleRestController(ArticleService articleService) {
+        this.articleService = articleService;
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<ArticleResponse> get(@PathVariable Long id) {
+        ArticleResponse articleResponse = articleService.getArticle(id);
+        return ResponseEntity.ok().body(articleResponse);
+    }
+}
